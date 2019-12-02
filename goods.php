983d2ff8fc832bf087789632c4020d032a4dc3a9
Timur Zhukotinskiy <tyzhukotinskiy@gmail.com>
2019-12-02 09:42:29 +0200

    Fill the file with goods and add goods pictures

---
+++ goods.php
@@ -0,0 +1,29 @@
+<?php
+	$goods = [
+		"good1" => [
+			"image" => "lego_clinick.jpg",
+			"name" => "lego clinick", 
+			"price" => "100", 
+			"description" => "Конструктор детский | Конструктор здание | Больница конструктор"
+		],
+		"good2" => [
+			"image" => "lego_minecraft.jpg",
+			"name" => "lego minecraft: zombie cave", 
+			"price" => "70", 
+			"description" => "Конструктор детский | Конструктор пещера | Майнкрафт лего конструктор"
+		],
+		"good3" => [
+			"image" => "lego_harry_potter.jpg",
+			"name" => "lego Harry Potter castle", 
+			"price" => "230", 
+			"description" => "Конструктор детский | Конструктор Замок | Гарри Поттер лего конструктор"
+		],
+		"good4" => [
+			"image" => "lego_car.jpg",
+			"name" => "lego Speed car", 
+			"price" => "40", 
+			"description" => "Конструктор детский | Конструктор Машина | Лего автомобиль"
+		]
+	];
+	print_r($goods);
+?>
